8d18a766c6a63cbfb857f93008d3de9f9af81c15
Manuel Munz <munz@comuno.net>
2018-10-13 18:18:08 +0200

    more useful RatioBoxTest

--- Tests/Functional/ViewHelpers/RatioBoxTest.php
+++ Tests/Functional/ViewHelpers/RatioBoxTest.php
@@ -21,21 +21,35 @@ class RatioBoxTest extends AbstractFunctionalTest
     /**
      * @test
      *
-     * Custom alt text is shown
+     * inline css style tag is added to the HEAD of the page with correct rules
      */
-    public function customAltTextIsShown()
+    public function cssIsAddedToHead()
     {
-        $properties = [
-            'alternative' => 'Using alt text is important for accessibility!'
-        ];
-
+        $properties = [];
         $this->createAndReturnFileReference('user_upload/nightlife-4.jpg', 'tt_content', 'assets', 1, 0, $properties);
-        $arguments = '&mode=testmode';
+        // @ToDo - can only pass strings for now, find out how to pass arrays
+        // $arguments = '&mq-mobile=(max-width:680px)';
+        $arguments = [];
         $response = $this->getFrontendResponse($this->pageId, 0, 0, 0, true, 0, $arguments);
 
-        $this::assertContains(
-            'Using alt text is important for accessibility!',
-            $response->getContent()
-        );
+        $expected = "<style type=\"text/css\">\n/*<![CDATA[*/\n<!-- \n/*rb--62dot5*/\n.rb--62dot5{padding-bottom:62.5%}\n-->\n/*]]>*/\n</style>\n";
+        $this::assertContains($expected, $response->getContent());
+    }
+
+    /**
+     * @test
+     *
+     * test that a ratio box wrapper element (per default a DIV) with correct values exists
+     */
+    public function ratioBoxWrapperExists()
+    {
+        $properties = [];
+        $this->createAndReturnFileReference('user_upload/nightlife-4.jpg', 'tt_content', 'assets', 1, 0, $properties);
+
+        $arguments = [];
+        $response = $this->getFrontendResponse($this->pageId, 0, 0, 0, true, 0, $arguments);
+
+        $expected = '<div class="rb rb--62dot5 rb--62dot5"><img';
+        $this::assertContains($expected, $response->getContent());
     }
 }
