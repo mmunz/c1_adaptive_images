5102151946b3c489fffb5f04be16f9f7601169fd
Manuel Munz <munz@comuno.net>
2018-12-23 09:48:56 +0100

    add tests for multiple sources and complex media queries for pictureViewHelper

--- Tests/Acceptance/PictureViewHelperCest.php
+++ Tests/Acceptance/PictureViewHelperCest.php
@@ -13,6 +13,7 @@ class PictureViewHelperCest
         $I->pauseExecution();
     }
 
+
     public function seePictureLoadInCorrectDimensions(\AcceptanceTester $I)
     {
         $I->flushCache();
@@ -61,6 +62,46 @@ class PictureViewHelperCest
         $I->expect('a 640px image is loaded. Ratio is odd because of rounding errors but close to 4:3.');
         $I->seeCurrentImageDimensions(640, 479, '74.84');
         $I->seeRatioBoxHasPaddingBottom(0, '.rb.rb--max-width767px-74dot86', '74.86%');
+
+        $I->resizeWindow(1024, 768);
+        $I->waitForImagesLoaded();
+
+        $I->expect('a 1024px image is loaded with 16:9 ratio.');
+        $I->seeCurrentImageDimensions(1024, 640, '62.50');
+        $I->seeRatioBoxHasPaddingBottom(0, '.rb.rb--62dot5', '62.5%');
+    }
+
+    public function seePictureLoadInCorrectDimensionsWithMultipleSourcesAndRatioBox(\AcceptanceTester $I)
+    {
+        $I->flushCache();
+        $properties = [
+            'crop' => '{"default":{"cropArea":{"x":0,"y":0,"width":1,"height":1},"selectedRatio":"NaN"}, "mobile":{"cropArea":{"height":0.624,"width":0.521,"x":0,"y":0},"selectedRatio":"4:3"}, "tablet":{"cropArea":{"height":0.624,"width":0.521,"x":0,"y":0},"selectedRatio":"4:3"}}'
+        ];
+        $I->updateInDatabase('sys_file_reference', $properties, ['uid' => 1]);
+
+        $I->amOnPage('/index.php?mode=PictureViewHelperWithMultipleSources&srcsetWidths=640,1024&debug=1&lazy=0&ratiobox=1');
+
+        $I->expect('Page has valid markup.');
+        /* style in head inside CDATA is htmlspecialchar'ed by webdriver which causes validation to fail, see
+         * https://github.com/seleniumhq/selenium-google-code-issue-archive/issues/4264
+         * Workaround for now: ignore CSS errors in test
+         */
+        $I->validateMarkup([
+            'ignoredErrors' => [
+                '/CSS: Parse Error./',
+            ],
+        ]);
+
+        $I->expect('a 640px image is loaded. Ratio is odd because of rounding errors but close to 4:3.');
+        $I->seeCurrentImageDimensions(640, 479, '74.84');
+        $I->seeRatioBoxHasPaddingBottom(0, '.rb.rb--max-width767px-74dot86', '74.86%');
+
+        $I->resizeWindow(992, 768);
+        $I->waitForImagesLoaded();
+
+        $I->expect('a 992px image is loaded with 4:3 ratio.');
+        $I->seeCurrentImageDimensions(992, 743, '74.90');
+        $I->seeRatioBoxHasPaddingBottom(0, '.rb.rb--min-width768pxandmax-width992px-74dot86', '74.86%');
 
         $I->resizeWindow(1024, 768);
         $I->waitForImagesLoaded();
