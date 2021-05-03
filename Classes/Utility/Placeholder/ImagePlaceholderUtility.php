da76eed4923e6ade2c2e6b571f6c9b3760cbce2b
Tim Dreier <dreier@team23.de>
2021-05-03 10:34:53 +0200

    Fixes mmunz/c1_adaptive_images#15

--- Classes/Utility/Placeholder/ImagePlaceholderUtility.php
+++ Classes/Utility/Placeholder/ImagePlaceholderUtility.php
@@ -65,19 +65,35 @@ class ImagePlaceholderUtility
                 '-unsharp 0.25x0.25+8+0.065 -despeckle -noise 5'
         ];
         $processedImage = $this->imageService->applyProcessingInstructions($file, $processingInstructions);
-        if ($processedImage->exists()) {
+
+        if ($processedImage->getProperties()['width'] != $width) {
+            $pixel = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII=';
+
             if ($base64 === false) {
-                $imageUri = $this->imageService->getImageUri($processedImage, $absolute);
+                $imageUri = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('c1_adaptive_images') . 'Resources/Public/Images/placeholder.png';
                 return $imageUri;
             } else {
                 return sprintf(
                     'data:%s;base64,%s',
-                    $file->getProperty('mime_type'),
-                    base64_encode($processedImage->getContents())
+                    'image/png',
+                    $pixel
                 );
             }
         } else {
-            return $processedImage->getPublicUrl();
+            if ($processedImage->exists()) {
+                if ($base64 === false) {
+                    $imageUri = $this->imageService->getImageUri($processedImage, $absolute);
+                    return $imageUri;
+                } else {
+                    return sprintf(
+                        'data:%s;base64,%s',
+                        $file->getProperty('mime_type'),
+                        base64_encode($processedImage->getContents())
+                    );
+                }
+            } else {
+                return $processedImage->getPublicUrl();
+            }
         }
     }
 }
