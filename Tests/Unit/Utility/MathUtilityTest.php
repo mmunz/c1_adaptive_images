69929903e1dfe2b6c9c8c24ff2c4b77d30751ba0
bambamboole <manuel.christlieb@xentral.com>
2022-06-02 21:07:01 +0200

    Add test for log warning in MathUtility

--- Tests/Unit/Utility/MathUtilityTest.php
+++ Tests/Unit/Utility/MathUtilityTest.php
@@ -4,6 +4,7 @@ namespace C1\AdaptiveImages\Tests\Unit\Utility;
 
 use C1\AdaptiveImages\Utility\MathUtility;
 use Nimut\TestingFramework\TestCase\UnitTestCase;
+use Psr\Log\LoggerInterface;
 
 /**
  * Class MathUtilityTest
@@ -20,5 +21,18 @@ class MathUtilityTest extends UnitTestCase
         $this->assertEquals(100, $utility->calculateRatio(400, 400));
         $this->assertEquals(50, $utility->calculateRatio(200, 400));
         $this->assertEquals(21.77, $utility->calculateRatio(100.25, 460.5));
+    }
+
+    public function testNotSetValuesLeadToLoggedWarning(): void
+    {
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->expects(self::exactly(4))->method('warning');
+        $utility = new MathUtility();
+        $utility->setLogger($logger);
+
+        $utility->calculateRatio(0, 0);
+        $utility->calculateRatio(100, 0);
+        $utility->calculateRatio(0, 100);
+        $utility->calculateRatio(0, 100);
     }
 }
